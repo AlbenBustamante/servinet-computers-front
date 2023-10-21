e47097e03b0fa09e4a81b904a723f22830df1ef4
Alben <albenbustamante@gmail.com>
2023-10-20 19:24:12 -0500

    update: the platform service is used on the new transfer form

--- src/app/modules/home/components/new-transfer-form/new-transfer-form.component.ts
+++ src/app/modules/home/components/new-transfer-form/new-transfer-form.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPlatformRes } from 'src/app/core/models/platform.model';
+import { PlatformService } from 'src/app/core/services/platform.service';
 
 @Component({
   selector: 'app-new-transfer-form',
   templateUrl: './new-transfer-form.component.html',
-  styleUrls: ['./new-transfer-form.component.css']
+  styleUrls: ['./new-transfer-form.component.css'],
 })
-export class NewTransferFormComponent {
+export class NewTransferFormComponent implements OnInit {
+  platforms: IPlatformRes[];
+  numbers: number[] = [];
+  private readonly maxAmount: number = 10;
 
+  constructor(private readonly platformService: PlatformService) {
+    this.platforms = this.platformService.getAll();
+  }
+
+  ngOnInit(): void {
+    for (let i = 1; i <= this.maxAmount; i++) {
+      this.numbers.push(i);
+    }
+  }
 }
