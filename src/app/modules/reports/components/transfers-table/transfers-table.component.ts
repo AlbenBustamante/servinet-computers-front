b406eb417e3588eea2228ce77c0f4de94cb2e8f0
Alben <albenbustamante@gmail.com>
2023-10-21 15:39:38 -0500

    implemented the transfer service to the transfers-table component

--- src/app/modules/reports/components/transfers-table/transfers-table.component.ts
+++ src/app/modules/reports/components/transfers-table/transfers-table.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ITransferRes } from 'src/app/core/models/transfer.model';
+import { TransferService } from 'src/app/core/services/transfer.service';
 
 @Component({
   selector: 'app-transfers-table',
   templateUrl: './transfers-table.component.html',
-  styleUrls: ['./transfers-table.component.css']
+  styleUrls: ['./transfers-table.component.css'],
 })
 export class TransfersTableComponent {
+  transfers: ITransferRes[];
 
+  constructor(private readonly transferService: TransferService) {
+    this.transfers = this.transferService.getAll();
+  }
 }
