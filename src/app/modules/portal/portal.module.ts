633d42f6f711ce5e994617a616aa4a1d237f3fd4
Alben <albenbustamante@gmail.com>
2023-10-27 17:13:17 -0500

    add: temporary design for the form and images

--- src/app/modules/portal/portal.module.ts
+++ src/app/modules/portal/portal.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PortalRoutingModule } from './portal-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, LoginFormComponent],
   imports: [CommonModule, PortalRoutingModule, SharedModule],
 })
 export class PortalModule {}
