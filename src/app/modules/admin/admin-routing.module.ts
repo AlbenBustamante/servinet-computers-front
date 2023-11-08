6ba73d26a09a6433e48a667f99d089fa9ccf4d32
Alben <albenbustamante@gmail.com>
2023-11-08 15:52:43 -0500

    add: new portal page for the admins (role user)

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { PortalComponent } from './pages/portal/portal.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'portal',
     pathMatch: 'full',
   },
+  { path: 'portal', component: PortalComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -6,9 +6,15 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { SharedModule } from '../shared/shared.module';
+import { PortalComponent } from './pages/portal/portal.component';
 
 @NgModule({
-  declarations: [LoginFormComponent, LoginComponent, RegisterComponent],
+  declarations: [
+    LoginFormComponent,
+    LoginComponent,
+    RegisterComponent,
+    PortalComponent,
+  ],
   imports: [CommonModule, AdminRoutingModule, SharedModule],
 })
 export class AdminModule {}
