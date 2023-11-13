a351dc945b2c70e3de7b101c2f2714dbb6f665b4
Alben Bustamante <albenbustamante@gmail.com>
2023-11-13 15:35:25 -0500

    remove: admin's register page

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { SharedModule } from '../shared/shared.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -15,7 +14,6 @@ import { CampusesComponent } from './components/campuses/campuses.component';
   declarations: [
     LoginFormComponent,
     LoginComponent,
-    RegisterComponent,
     NavbarComponent,
     DashboardComponent,
     StatsComponent,
