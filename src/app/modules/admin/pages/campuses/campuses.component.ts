20ead63966e79924d90249dfb6749a7df299e112
Alben <albenbustamante@gmail.com>
2023-11-23 16:36:42 -0500

    feat: connect the campus' table with the service

--- src/app/modules/admin/pages/campuses/campuses.component.ts
+++ src/app/modules/admin/pages/campuses/campuses.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { ICampusRes } from 'src/app/core/models/campus.model';
+import { CampusService } from 'src/app/core/services/campus.service';
 
 @Component({
   selector: 'app-campuses-page',
   templateUrl: './campuses.component.html',
   styleUrls: ['./campuses.component.css'],
 })
-export class CampusesComponent {}
+export class CampusesComponent {
+  campuses: ICampusRes[];
+
+  constructor(private readonly campusService: CampusService) {
+    this.campuses = this.campusService.getAll();
+  }
+}
