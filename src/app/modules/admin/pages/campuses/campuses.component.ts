9bb01256d2c9e367ef201bd8818b0f42c9c365de
Alben Bustamante <albenbustamante@gmail.com>
2023-11-26 18:11:19 -0500

    feat: create temporary modal

--- src/app/modules/admin/pages/campuses/campuses.component.ts
+++ src/app/modules/admin/pages/campuses/campuses.component.ts
@@ -10,12 +10,27 @@ import { CampusService } from 'src/app/core/services/campus.service';
 export class CampusesComponent {
   campuses: ICampusRes[];
   isRegistering: boolean = false;
+  modal: boolean = false;
+  modalData!: ICampusRes;
 
   constructor(private readonly campusService: CampusService) {
     this.campuses = this.campusService.getAll();
+
+    if (this.campuses) {
+      this.modalData = this.campuses[0];
+    }
   }
 
   setIsRegistering() {
     this.isRegistering = !this.isRegistering;
+  }
+
+  openModal(campus: ICampusRes) {
+    this.modal = true;
+    this.modalData = campus;
+  }
+
+  closeModal() {
+    this.modal = false;
   }
 }
