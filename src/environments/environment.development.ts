4a5c4ded31ae915fa3aa9857ca968e98a9e8e448
Alben Bustamante <albenbustamante@gmail.com>
2023-12-03 19:57:14 -0500

    add: environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://sc-api.fly.dev/api/v1',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://sc-api.fly.dev/api/v1',
+};
