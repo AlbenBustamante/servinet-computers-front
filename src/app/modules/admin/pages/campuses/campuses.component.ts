d20638c485c47a808d1f904031e3112bfad652c9
Alben Bustamante <albenbustamante@gmail.com>
2023-12-06 23:10:18 -0500

    feat: new campuses empty message and update header title

--- src/app/modules/admin/pages/campuses/campuses.component.ts
+++ src/app/modules/admin/pages/campuses/campuses.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ICampusRes } from 'src/app/core/models/campus.model';
-import { CampusService } from 'src/app/core/services/campus.service';
 import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
@@ -9,26 +8,39 @@ import { UserService } from 'src/app/core/services/user.service';
   styleUrls: ['./campuses.component.css'],
 })
 export class CampusesComponent {
-  campuses!: ICampusRes[];
+  campuses: ICampusRes[] = [];
   isRegistering: boolean = false;
   modal: boolean = false;
-  modalData!: ICampusRes;
+  headerTitle: string = 'Sedes registradas';
+  modalData: ICampusRes;
 
-  constructor(
-    private readonly userService: UserService,
-    private readonly campusService: CampusService
-  ) {
+  constructor(private readonly userService: UserService) {
     this.userService
       .getCampuses(1)
       .subscribe((res) => (this.campuses = res.data.results));
 
-    if (this.campuses) {
+    if (this.campuses.length > 0) {
       this.modalData = this.campuses[0];
+    } else {
+      this.modalData = {
+        address: '',
+        available: false,
+        cellphone: '',
+        createdAt: new Date(),
+        id: 0,
+        numeral: -1,
+        platforms: [],
+        terminal: '0',
+        updatedAt: new Date(),
+      };
     }
   }
 
   setIsRegistering() {
     this.isRegistering = !this.isRegistering;
+    this.headerTitle = this.isRegistering
+      ? 'Registro de nueva sede'
+      : 'Sedes registradas';
   }
 
   openModal(campus: ICampusRes) {
