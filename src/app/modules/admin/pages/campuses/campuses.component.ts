c84ada1d5aac583885e4739529287d97d9104346
Alben Bustamante <albenbustamante@gmail.com>
2023-12-08 20:25:22 -0500

    create: temporary dialog

--- src/app/modules/admin/pages/campuses/campuses.component.ts
+++ src/app/modules/admin/pages/campuses/campuses.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ICampusRes } from 'src/app/core/models/campus.model';
+import { IPlatformRes } from 'src/app/core/models/platform.model';
 import { CampusService } from 'src/app/core/services/campus.service';
+import { PlatformService } from 'src/app/core/services/platform.service';
 import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
@@ -11,6 +13,7 @@ import { UserService } from 'src/app/core/services/user.service';
 })
 export class CampusesComponent {
   campuses: ICampusRes[] = [];
+  platforms: IPlatformRes[] = [];
   isRegistering: boolean = false;
   modal: boolean = false;
   headerTitle: string = 'Sedes registradas';
@@ -30,6 +33,7 @@ export class CampusesComponent {
   constructor(
     private readonly userService: UserService,
     private readonly campusService: CampusService,
+    private readonly platformService: PlatformService,
     private readonly fb: FormBuilder
   ) {
     this.form = this.fb.group({
@@ -43,6 +47,10 @@ export class CampusesComponent {
     this.userService
       .getCampuses(3) // temporal
       .subscribe((res) => (this.campuses = res.data.results));
+
+    this.platformService
+      .getAll()
+      .subscribe((res) => (this.platforms = res.data.results));
   }
 
   setIsRegistering() {
