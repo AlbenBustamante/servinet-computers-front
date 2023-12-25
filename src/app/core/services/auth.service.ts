04fa3319ba571d34796b5a59e30c8db1584976eb
Alben Bustamante <albenbustamante@gmail.com>
2023-12-25 18:09:46 -0500

    update: login method to save the token

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { IAuthRequest, IAuthResponse } from '../models/auth.model';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root',
@@ -10,9 +12,14 @@ import { Observable } from 'rxjs';
 export class AuthService {
   private readonly url: string = `${environment.apiUrl}/auth`;
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly tokenService: TokenService
+  ) {}
 
   login(req: IAuthRequest): Observable<IAuthResponse> {
-    return this.http.post<IAuthResponse>(`${this.url}/sign-in`, req);
+    return this.http
+      .post<IAuthResponse>(`${this.url}/sign-in`, req)
+      .pipe(tap((res) => this.tokenService.save(res.jwt)));
   }
 }
