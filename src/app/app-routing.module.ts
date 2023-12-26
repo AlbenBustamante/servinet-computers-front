08f2f1472b6d13436e1a81ef4f745dfd4b2deb45
Alben Bustamante <albenbustamante@gmail.com>
2023-12-25 19:23:35 -0500

    feat: create temporary redirect guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
+import { redirectGuard } from './core/guards/redirect.guard';
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
   },
   {
     path: 'portal',
+    canActivate: [redirectGuard('/')],
     loadChildren: () =>
       import('./modules/portal/portal.module').then((m) => m.PortalModule),
   },
--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PlatformsComponent } from './pages/platforms/platforms.component';
 import { CampusesComponent } from './pages/campuses/campuses.component';
 import { authGuard } from 'src/app/core/guards/auth.guard';
+import { redirectGuard } from 'src/app/core/guards/redirect.guard';
 
 const loginPath: string = '/admin/login';
 
@@ -14,7 +15,11 @@ const routes: Routes = [
     redirectTo: 'dashboard',
     pathMatch: 'full',
   },
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [redirectGuard('/admin/')],
+  },
   {
     path: 'dashboard',
     component: DashboardComponent,
