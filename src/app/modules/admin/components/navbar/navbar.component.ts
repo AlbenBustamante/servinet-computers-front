227c3337652bce0dbf4e5ceac2bfbbe07f0950cf
Alben Bustamante <albenbustamante@gmail.com>
2023-12-28 21:09:59 -0500

    feat: connect logout button with api

--- src/app/modules/admin/components/navbar/navbar.component.ts
+++ src/app/modules/admin/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
 
+  logout() {
+    this.authService.logout().subscribe({
+      next: () => this.router.navigateByUrl('/admin/login'),
+    });
+  }
 }
