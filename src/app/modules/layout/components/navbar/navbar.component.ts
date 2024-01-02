38e754274293d6314b76ada5ede818891361eed4
Alben Bustamante <albenbustamante@gmail.com>
2024-01-01 20:35:13 -0500

    feat: update cursor and connect logout method with api

--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: (ok) => {
+        if (ok) {
+          this.router.navigateByUrl('/portal');
+        }
+      },
+    });
+  }
+}
