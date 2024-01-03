f85e40f99c4b2d6a4739630f624547ef6962c32b
Alben Bustamante <albenbustamante@gmail.com>
2024-01-02 23:23:40 -0500

    feat: design dashboard layout

--- src/app/modules/admin/pages/dashboard/dashboard.component.ts
+++ src/app/modules/admin/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IRoute } from 'src/app/core/models/route.model';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -7,6 +8,11 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  routes: IRoute[] = [
+    { title: 'Admin', icon: 'home', route: '/admin' },
+    { title: 'Panel de usuario' },
+  ];
+
   constructor(private readonly authService: AuthService) {}
 
   ngOnInit(): void {
