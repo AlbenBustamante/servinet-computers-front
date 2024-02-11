398a8e4d276923b1c694b85f950f04d98ce9e888
Alben Bustamante <albenbustamante@gmail.com>
2024-02-11 10:20:19 -0500

    feature: add validations and loadings to platforms page and fix ssl protocol

--- src/app/modules/admin/pages/platforms/platforms.component.ts
+++ src/app/modules/admin/pages/platforms/platforms.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IPlatformRes } from 'src/app/core/models/platform.model';
+import { RequestStatus } from 'src/app/core/models/request-status.model';
 import { IRoute } from 'src/app/core/models/route.model';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { PlatformService } from 'src/app/core/services/platform.service';
+import { GeneralValidators } from 'src/app/core/utils/general-validators';
 
 @Component({
   selector: 'app-platforms',
@@ -27,15 +29,24 @@ export class PlatformsComponent implements OnInit {
   };
   newPlatformModal!: HTMLDialogElement;
   form: FormGroup;
+  platformsStatus: RequestStatus = 'loading';
+  formStatus: RequestStatus = 'init';
 
   constructor(
     private readonly platformService: PlatformService,
     private readonly authService: AuthService,
-    private readonly formBuilder: FormBuilder
+    private readonly formBuilder: FormBuilder,
+    private readonly validator: GeneralValidators
   ) {
-    this.platformService
-      .getAll()
-      .subscribe((res) => (this.platforms = res.data.results));
+    this.platformService.getAll().subscribe({
+      next: (res) => {
+        this.platforms = res.data.results;
+        this.platformsStatus = 'success';
+      },
+      error: (error) => {
+        this.platformsStatus = 'failed';
+      },
+    });
 
     this.form = this.formBuilder.group({
       name: ['', Validators.required],
@@ -59,12 +70,20 @@ export class PlatformsComponent implements OnInit {
   }
 
   onSubmit() {
+    if (this.form.invalid) {
+      return this.form.markAllAsTouched();
+    }
+
+    this.modal.close();
+    this.formStatus = 'loading';
+
     this.platformService.register(this.form.value).subscribe({
       next: (res) => {
+        this.formStatus = 'success';
         this.platforms.push(res.data.results[0]);
-        this.modal.close();
       },
       error: (err) => {
+        this.formStatus = 'failed';
         console.log(err);
       },
     });
@@ -82,5 +101,9 @@ export class PlatformsComponent implements OnInit {
     }
 
     return this.newPlatformModal;
+  }
+
+  hasError(controlName: string, error: string) {
+    return this.validator.hasError(this.form, controlName, error);
   }
 }
