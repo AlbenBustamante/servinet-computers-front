bd7315de344e31fcb510fc5caf1f0fad6089d62d
Alben Bustamante <albenbustamante@gmail.com>
2024-02-26 18:51:41 -0500

    create: little layout to show the balances

--- src/app/modules/portal/pages/balances/balances.component.ts
+++ src/app/modules/portal/pages/balances/balances.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPlatformRes } from '@models/platform.model';
 import { IRoute } from '@models/route.model';
+import { CampusService } from '@services/campus.service';
 
 @Component({
   selector: 'app-balances',
   templateUrl: './balances.component.html',
   styleUrls: ['./balances.component.css'],
 })
-export class BalancesComponent {
-  routes: IRoute[] = [
-    {
-      icon: 'account_balance',
-      title: 'Saldos',
-      route: '/balances',
-    },
-  ];
+export class BalancesComponent implements OnInit {
+  platforms: IPlatformRes[] = [];
+
+  constructor(private readonly campusService: CampusService) {}
+
+  ngOnInit(): void {
+    this.campusService.platforms$.subscribe({
+      next: (res) => {
+        this.platforms = res;
+      },
+    });
+  }
 }
--- src/app/modules/portal/pages/balances/balances.module.ts
+++ src/app/modules/portal/pages/balances/balances.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BalancesRoutingModule } from './balances-routing.module';
 import { BalancesComponent } from './balances.component';
-
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [
-    BalancesComponent
-  ],
-  imports: [
-    CommonModule,
-    BalancesRoutingModule
-  ]
+  declarations: [BalancesComponent],
+  imports: [CommonModule, BalancesRoutingModule, SharedModule],
 })
-export class BalancesModule { }
+export class BalancesModule {}
