fbdae404edf0489f165c1fbab99aa7c2689b1285
Alben Bustamante <albenbustamante@gmail.com>
2024-02-26 19:02:39 -0500

    feat: map balances into template

--- src/app/modules/portal/pages/balances/balances.component.ts
+++ src/app/modules/portal/pages/balances/balances.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { IPlatformRes } from '@models/platform.model';
-import { IRoute } from '@models/route.model';
+import { IBalanceRes } from '@models/balance.model';
+import { RequestStatus } from '@models/request-status.model';
 import { CampusService } from '@services/campus.service';
 
 @Component({
@@ -9,14 +9,21 @@ import { CampusService } from '@services/campus.service';
   styleUrls: ['./balances.component.css'],
 })
 export class BalancesComponent implements OnInit {
-  platforms: IPlatformRes[] = [];
+  balances: IBalanceRes[] = [];
+  balancesStatus: RequestStatus = 'init';
 
   constructor(private readonly campusService: CampusService) {}
 
   ngOnInit(): void {
-    this.campusService.platforms$.subscribe({
+    this.balancesStatus = 'loading';
+
+    this.campusService.getBalances().subscribe({
       next: (res) => {
-        this.platforms = res;
+        this.balances = res.data.results;
+        this.balancesStatus = 'success';
+      },
+      error: () => {
+        this.balancesStatus = 'failed';
       },
     });
   }
