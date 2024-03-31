d2f2f2bddbc2c4c0631001e2e93e3fcbe5817cfb
Alben Bustamante <albenbustamante@gmail.com>
2024-03-31 16:03:52 -0500

    feat: link platforms list with api and show empty list message

--- src/app/modules/admin/pages/platforms/components/platforms-list/platforms-list.component.ts
+++ src/app/modules/admin/pages/platforms/components/platforms-list/platforms-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IPlatformRes } from '@models/platform.model';
+import { PlatformService } from '@services/platform.service';
 
 @Component({
   selector: 'app-platforms-list',
   templateUrl: './platforms-list.component.html',
-  styleUrls: ['./platforms-list.component.css']
+  styleUrls: ['./platforms-list.component.css'],
 })
 export class PlatformsListComponent {
+  readonly loading = signal<boolean>(true);
+  readonly platforms = signal<IPlatformRes[]>([]);
 
+  constructor(private readonly platformService: PlatformService) {}
+
+  ngOnInit() {
+    this.platformService.getAll().subscribe({
+      next: (res) => {
+        this.loading.set(false);
+        this.platforms.set(res);
+      },
+      error: (error) => {
+        this.loading.set(false);
+        console.log(error);
+      },
+    });
+  }
 }
