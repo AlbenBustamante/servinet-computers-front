7486879cdf3144be30e24d56571b1e47f26d4995
Alben Bustamante <albenbustamante@gmail.com>
2024-03-31 17:37:49 -0500

    feat: define role pipe

---
+++ src/app/core/pipes/role.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Role } from '@models/enums';
+
+@Pipe({
+  name: 'role',
+  standalone: true,
+})
+export class RolePipe implements PipeTransform {
+  transform(role: Role): string {
+    if (role === Role.CASHIER) {
+      return 'Cajero';
+    }
+
+    if (role === Role.SUPERVISOR) {
+      return 'Supervisor';
+    }
+
+    if (role === Role.ADMIN) {
+      return 'Administrador';
+    }
+
+    return 'Desconocido';
+  }
+}
--- src/app/modules/admin/pages/users/users.module.ts
+++ src/app/modules/admin/pages/users/users.module.ts
@@ -5,9 +5,10 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 import { SharedModule } from '@shared/shared.module';
 import { UsersListComponent } from './components/users-list/users-list.component';
+import { RolePipe } from 'app/core/pipes/role.pipe';
 
 @NgModule({
   declarations: [UsersComponent, UsersListComponent],
-  imports: [CommonModule, UsersRoutingModule, SharedModule],
+  imports: [CommonModule, UsersRoutingModule, SharedModule, RolePipe],
 })
 export default class UsersModule {}
