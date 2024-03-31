6463b2e0d60ae3e4c4f24e1655c2905f843e74a0
Alben Bustamante <albenbustamante@gmail.com>
2024-03-31 15:49:58 -0500

    refactor: create new platform form component

--- src/app/modules/admin/pages/platforms/platforms.module.ts
+++ src/app/modules/admin/pages/platforms/platforms.module.ts
@@ -5,9 +5,10 @@ import { PlatformsRoutingModule } from './platforms-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { PlatformsComponent } from './platforms.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NewPlatformFormComponent } from './components/new-platform-form/new-platform-form.component';
 
 @NgModule({
-  declarations: [PlatformsComponent],
+  declarations: [PlatformsComponent, NewPlatformFormComponent],
   imports: [
     CommonModule,
     PlatformsRoutingModule,
