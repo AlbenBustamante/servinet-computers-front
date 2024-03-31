8bc14d7f16bc735aabf1d96320a0bd23c144c996
Alben Bustamante <albenbustamante@gmail.com>
2024-03-30 23:47:22 -0500

    feat: pint header bar and load loggedIn on admin pages

--- src/app/modules/admin/admin.component.ts
+++ src/app/modules/admin/admin.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent {
+  constructor(private readonly authService: AuthService) {}
 
+  ngOnInit() {
+    this.authService.getLoggedIn().subscribe();
+  }
 }
