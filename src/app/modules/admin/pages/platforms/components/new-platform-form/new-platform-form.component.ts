ce051a1e78ba88dd33f7a9024185957e636d359b
Alben Bustamante <albenbustamante@gmail.com>
2024-03-31 16:08:25 -0500

    feat: link new platform form with api

--- src/app/modules/admin/pages/platforms/components/new-platform-form/new-platform-form.component.ts
+++ src/app/modules/admin/pages/platforms/components/new-platform-form/new-platform-form.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PlatformService } from '@services/platform.service';
 
 @Component({
   selector: 'app-new-platform-form',
   templateUrl: './new-platform-form.component.html',
-  styleUrls: ['./new-platform-form.component.css']
+  styleUrls: ['./new-platform-form.component.css'],
 })
 export class NewPlatformFormComponent {
+  form: FormGroup;
+  readonly loading = signal<boolean>(false);
 
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly platformService: PlatformService
+  ) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return this.form.markAllAsTouched();
+    }
+
+    this.loading.set(true);
+
+    this.platformService.register(this.form.value).subscribe({
+      next: () => {
+        this.loading.set(false);
+        this.form.reset();
+      },
+      error: (error) => {
+        this.loading.set(false);
+        console.log(error);
+      },
+    });
+  }
 }
