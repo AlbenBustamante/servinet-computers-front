3bbac0765822162e99e71a68d03811c487b6079b
Alben Bustamante <albenbustamante@gmail.com>
2024-04-06 23:51:46 -0500

    feat: design new expense form

--- src/app/modules/portal/pages/home/home.module.ts
+++ src/app/modules/portal/pages/home/home.module.ts
@@ -7,9 +7,10 @@ import { SharedModule } from '@shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NewTransactionFormComponent } from './components/new-transaction-form/new-transaction-form.component';
 import { NewCashRegisterTransferFormComponent } from './components/new-cash-register-transfer-form/new-cash-register-transfer-form.component';
+import { NewExpenseFormComponent } from './components/new-expense-form/new-expense-form.component';
 
 @NgModule({
-  declarations: [HomeComponent, NewTransactionFormComponent, NewCashRegisterTransferFormComponent],
+  declarations: [HomeComponent, NewTransactionFormComponent, NewCashRegisterTransferFormComponent, NewExpenseFormComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export default class HomeModule {}
