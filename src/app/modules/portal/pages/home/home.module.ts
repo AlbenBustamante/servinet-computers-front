d219551ae78a005b690220755a1707e22e188f9c
Alben Bustamante <albenbustamante@gmail.com>
2024-04-06 23:34:19 -0500

    feat: create temporary design to create new transfer between cash registers

--- src/app/modules/portal/pages/home/home.module.ts
+++ src/app/modules/portal/pages/home/home.module.ts
@@ -6,9 +6,10 @@ import { HomeComponent } from './home.component';
 import { SharedModule } from '@shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NewTransactionFormComponent } from './components/new-transaction-form/new-transaction-form.component';
+import { NewCashRegisterTransferFormComponent } from './components/new-cash-register-transfer-form/new-cash-register-transfer-form.component';
 
 @NgModule({
-  declarations: [HomeComponent, NewTransactionFormComponent],
+  declarations: [HomeComponent, NewTransactionFormComponent, NewCashRegisterTransferFormComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export default class HomeModule {}
