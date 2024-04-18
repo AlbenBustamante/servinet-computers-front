5e33f05564980dfa27efc9b773912c3c10e70cf9
Alben <albenbustamante@gmail.com>
2024-04-18 11:58:05 -0500

    design: base calculator and change env url temporaly

--- src/app/core/services/base.service.ts
+++ src/app/core/services/base.service.ts
@@ -5,16 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class BaseService {
   readonly cashBase = [
-    { title: 'oneHundredThousand', value: 100000 },
+    { title: 'hundredThousand', value: 100000 },
     { title: 'fiftyThousand', value: 50000 },
     { title: 'twentyThousand', value: 20000 },
     { title: 'tenThousand', value: 10000 },
     { title: 'fiveThousand', value: 5000 },
     { title: 'twoThousand', value: 2000 },
-    { title: 'oneThousand', value: 1000 },
+    { title: 'thousand', value: 1000 },
     { title: 'fiveHundred', value: 500 },
     { title: 'twoHundred', value: 200 },
-    { title: 'oneHundred', value: 100 },
+    { title: 'hundred', value: 100 },
     { title: 'fifty', value: 50 },
   ];
 
