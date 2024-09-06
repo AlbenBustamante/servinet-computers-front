e22b87334fe671909e6cdcb1878d10c491aefbac
Alben Bustamante <albenbustamante@gmail.com>
2024-09-05 22:58:23 -0500

    update: logout logic

--- src/app/modules/shared/components/nav/nav.component.ts
+++ src/app/modules/shared/components/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { IRoute } from '@models/route.model';
 import { AuthService } from '@services/auth.service';
+import { TokenService } from '@services/token.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -14,11 +15,13 @@ export class NavComponent {
 
   constructor(
     private readonly authService: AuthService,
+    private readonly tokenService: TokenService,
     private readonly router: Router
   ) {}
 
   logout() {
-    this.authService.logout().subscribe();
+    // this.authService.logout().subscribe();
+    this.tokenService.remove();
     this.router.navigateByUrl('/login');
   }
 }
