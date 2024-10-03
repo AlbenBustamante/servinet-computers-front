19722fd7065b860d5d3aef05e688ccc440c11564
Alben Bustamante <albenbustamante@gmail.com>
2024-10-02 20:26:42 -0500

    update: final reports page

--- src/app/modules/portal/pages/cash-registers/pages/cash-register-closing/final-report/final-report.component.ts
+++ src/app/modules/portal/pages/cash-registers/pages/cash-register-closing/final-report/final-report.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { MyCashService } from '@services/my-cash.service';
 
 @Component({
   selector: 'app-final-report',
   templateUrl: './final-report.component.html',
-  styleUrls: ['./final-report.component.css']
+  styleUrls: ['./final-report.component.css'],
 })
 export class FinalReportComponent {
+  readonly reports;
 
+  constructor(private readonly myCashService: MyCashService) {
+    this.reports = this.myCashService.myClosedCashRegisterReports;
+  }
+
+  finish() {
+    this.myCashService.removeClosedReports();
+    this.myCashService.cashRegisterStatus.set('open'); // temporal
+  }
 }
--- src/app/modules/portal/pages/cash-registers/pages/cash-register-closing/final-report/final-report.module.ts
+++ src/app/modules/portal/pages/cash-registers/pages/cash-register-closing/final-report/final-report.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FinalReportRoutingModule } from './final-report-routing.module';
 import { FinalReportComponent } from './final-report.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [
-    FinalReportComponent
-  ],
-  imports: [CommonModule, FinalReportRoutingModule],
+  declarations: [FinalReportComponent],
+  imports: [CommonModule, FinalReportRoutingModule, SharedModule],
 })
 export default class FinalReportModule {}
