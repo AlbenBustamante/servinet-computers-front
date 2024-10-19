58e3922db3960109c94e703081296d9b3943d61d
Alben Bustamante <albenbustamante@gmail.com>
2024-10-19 18:53:30 -0500

    update: home info and platforms page description

--- src/app/modules/portal/pages/platforms/platforms.component.ts
+++ src/app/modules/portal/pages/platforms/platforms.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { Role } from '@models/enums';
 import { IPortalPlatform } from '@models/platform.model';
 import { PlatformService } from '@services/platform.service';
+import { TokenService } from '@services/token.service';
 
 @Component({
   selector: 'app-portal-platforms',
@@ -11,10 +13,21 @@ export class PlatformsComponent implements OnInit {
   readonly loading = signal<boolean>(false);
   readonly editing: WritableSignal<boolean>;
   readonly selectedPortalPlatform: WritableSignal<IPortalPlatform | null>;
+  readonly description;
 
-  constructor(private readonly platformService: PlatformService) {
+  constructor(
+    private readonly platformService: PlatformService,
+    private readonly tokenService: TokenService
+  ) {
     this.editing = this.platformService.editing;
     this.selectedPortalPlatform = this.platformService.selectedPortalPlatform;
+
+    const role = this.tokenService.getInfo().role;
+
+    this.description =
+      role === Role.SUPERVISOR
+        ? 'Eres libre de editarlos y añadir transferencias'
+        : 'Eres libre de añadir transferencias';
   }
 
   ngOnInit(): void {
