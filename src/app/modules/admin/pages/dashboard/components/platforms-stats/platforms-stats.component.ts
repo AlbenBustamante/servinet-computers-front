4d2085209ebe3e6c0073d7732f064b804f68ba2b
Alben Bustamante <albenbustamante@gmail.com>
2024-11-08 08:22:15 -0500

    feat: show platform stats as a dialog and update dashboard service

--- src/app/modules/admin/pages/dashboard/components/platforms-stats/platforms-stats.component.ts
+++ src/app/modules/admin/pages/dashboard/components/platforms-stats/platforms-stats.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { DashboardService } from '@services/dashboard.service';
 
 @Component({
   selector: 'app-platforms-stats',
   templateUrl: './platforms-stats.component.html',
-  styleUrls: ['./platforms-stats.component.css']
+  styleUrls: ['./platforms-stats.component.css'],
 })
 export class PlatformsStatsComponent {
+  readonly dashboard;
 
+  constructor(private readonly dashboardService: DashboardService) {
+    this.dashboard = this.dashboardService.dashboard;
+  }
 }
--- src/app/modules/admin/pages/dashboard/components/secondary-stats/secondary-stats.component.ts
+++ src/app/modules/admin/pages/dashboard/components/secondary-stats/secondary-stats.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DashboardService } from '@services/dashboard.service';
 
 @Component({
   selector: 'app-secondary-stats',
   templateUrl: './secondary-stats.component.html',
-  styleUrls: ['./secondary-stats.component.css']
+  styleUrls: ['./secondary-stats.component.css'],
 })
 export class SecondaryStatsComponent {
+  @ViewChild('platformsModal') platformsModal!: ElementRef<HTMLDialogElement>;
+  readonly dashboard;
 
+  constructor(private readonly dashboardService: DashboardService) {
+    this.dashboard = this.dashboardService.dashboard;
+  }
+
+  openPlatforms() {
+    this.platformsModal.nativeElement.showModal();
+  }
+
+  closePlatforms() {
+    this.platformsModal.nativeElement.close();
+  }
 }
