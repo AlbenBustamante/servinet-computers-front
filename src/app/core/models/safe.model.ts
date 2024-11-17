5172abb6c109918f3bd6a58e8b8b1cc96743c503
Alben Bustamante <albenbustamante@gmail.com>
2024-11-17 18:46:25 -0500

    temporal: safe changes

--- src/app/core/models/safe.model.ts
+++ src/app/core/models/safe.model.ts
@@ -1,16 +1,9 @@
-import { IBase } from './base.model';
 import { IResponse } from './response.model';
 
 export interface ISafeReq {
   numeral: number;
-  initialBase?: IBase;
-  finalBase?: IBase;
 }
 
 export interface ISafeRes extends IResponse {
   numeral: number;
-  initialBase: number;
-  finalBase: number;
-  detailInitialBase: IBase;
-  detailFinalBase: IBase;
 }
