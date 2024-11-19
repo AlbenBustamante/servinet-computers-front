89ecea9f4364b9d56acb2a3e68b57b954f385b05
Alben Bustamante <albenbustamante@gmail.com>
2024-11-19 17:28:54 -0500

    feat: link new api endpoint with safe service

--- src/app/core/models/safe.model.ts
+++ src/app/core/models/safe.model.ts
@@ -1,3 +1,4 @@
+import { IBase } from './base.model';
 import { IResponse } from './response.model';
 
 export interface ISafeReq {
@@ -6,4 +7,13 @@ export interface ISafeReq {
 
 export interface ISafeRes extends IResponse {
   numeral: number;
+}
+
+export interface ISafeDetailRes extends IResponse {
+  detailInitialBase: IBase;
+  detailFinalBase: IBase;
+  safeId: number;
+  initialBase: number;
+  finalBase: number;
+  calculatedBase: number;
 }
