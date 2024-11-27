c35df517b238d78bcd6473d93055efde3cc36e00
Alben Bustamante <albenbustamante@gmail.com>
2024-11-27 18:19:37 -0500

    feat: link update safe base endpoint with safe base calculator

--- src/app/modules/admin/pages/administration/pages/safes/components/update-base-form/update-base-form.component.ts
+++ src/app/modules/admin/pages/administration/pages/safes/components/update-base-form/update-base-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { IBase } from '@models/base.model';
 import { BaseService } from '@services/base.service';
+import { SafeService } from '@services/safe.service';
 import { UpdateSafeBaseService } from '@services/update-safe-base.service';
 
 @Component({
@@ -15,15 +17,31 @@ export class UpdateBaseFormComponent {
 
   constructor(
     private readonly baseService: BaseService,
-    private readonly updateSafeBaseService: UpdateSafeBaseService
+    private readonly updateSafeBaseService: UpdateSafeBaseService,
+    private readonly safeService: SafeService
   ) {
     this.base = signal(this.baseService.cashBase);
 
-    this.safeDetail = this.updateSafeBaseService.setSelectedSafe;
+    this.safeDetail = this.updateSafeBaseService.selectedSafe;
     this.form = this.updateSafeBaseService.form;
   }
 
   onSubmit() {
-    console.log(this.form.value);
+    if (this.form.invalid) {
+      return this.form.markAllAsTouched();
+    }
+
+    const safeDetailId = this.safeDetail()!.id;
+    const base = this.form.value as IBase;
+
+    this.safeService.updateBase({ safeDetailId, base }).subscribe({
+      next: (safeDetail) => {
+        this.safeDetail.set(safeDetail);
+        console.log(safeDetail);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
