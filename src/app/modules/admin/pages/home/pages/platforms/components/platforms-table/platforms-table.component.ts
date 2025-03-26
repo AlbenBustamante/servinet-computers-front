e5e2642cffd981607af68a44603f1d7bed5f6762
Alben Bustamante <albenbustamante@gmail.com>
2025-03-26 16:48:08 -0500

    temporal: actions on platforms

--- src/app/modules/admin/pages/home/pages/platforms/components/platforms-table/platforms-table.component.ts
+++ src/app/modules/admin/pages/home/pages/platforms/components/platforms-table/platforms-table.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { PlatformService } from '@services/platform.service';
 import { ITable } from '@shared/components/custom-table/custom-table.component';
 
@@ -19,10 +19,29 @@ export class PlatformsTableComponent {
         pipe: new DatePipe('es-CO'),
         pipeArgs: 'shortDateTime',
       },
+      {
+        key: 'modifiedDate',
+        title: 'Fecha de actualización',
+        pipe: new DatePipe('es-CO'),
+        pipeArgs: 'shortDateTime',
+      },
+      {},
     ],
     body: this.platformService.platforms,
     noDataMessage: 'Sin plataformas registradas...',
+    onEdit: (index) => this.onEdit(index),
+    onRemove: (index) => this.onRemove(index),
   };
 
   constructor(private readonly platformService: PlatformService) {}
+
+  onEdit(index: number) {
+    const platform = this.platformService.platforms()[index];
+    console.log({ on: 'Edit', json: platform });
+  }
+
+  onRemove(index: number) {
+    const platform = this.platformService.platforms()[index];
+    console.log({ on: 'Remove', json: platform });
+  }
 }
