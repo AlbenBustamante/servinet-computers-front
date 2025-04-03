c7b6cc03003a798137a25f9d697a4b7e1a3c1c8b
Alben Bustamante <albenbustamante@gmail.com>
2025-04-03 14:15:06 -0500

    feat: link tempcode endpoint with admin homepage

--- src/app/modules/admin/pages/home/home.component.ts
+++ src/app/modules/admin/pages/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import {
   faCashRegister,
   faHardDrive,
   faList,
   faUser,
 } from '@fortawesome/free-solid-svg-icons';
+import { ITempCodeRes } from '@models/temp-code.model';
+import { TempCodeService } from '@services/temp-code.service';
 
 @Component({
   selector: 'app-admin-home',
@@ -12,8 +14,18 @@ import {
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  readonly tempCode = signal<ITempCodeRes | undefined>(undefined);
   readonly faPlatforms = faList;
   readonly faCashRegister = faCashRegister;
   readonly faCashSafe = faHardDrive;
   readonly faUser = faUser;
+
+  constructor(private readonly tempCodeService: TempCodeService) {}
+
+  ngOnInit() {
+    this.tempCodeService.loadTempCode().subscribe({
+      next: (tempCode) => this.tempCode.set(tempCode),
+      error: (err) => console.log(err),
+    });
+  }
 }
