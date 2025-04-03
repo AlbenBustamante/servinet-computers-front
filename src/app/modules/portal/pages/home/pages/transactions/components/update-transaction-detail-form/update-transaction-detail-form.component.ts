b857f4af04636c0970c56cea16c21751f3db0236
Alben Bustamante <albenbustamante@gmail.com>
2025-04-03 17:13:26 -0500

    add: transactions list to update transaction detail form

--- src/app/modules/portal/pages/home/pages/transactions/components/update-transaction-detail-form/update-transaction-detail-form.component.ts
+++ src/app/modules/portal/pages/home/pages/transactions/components/update-transaction-detail-form/update-transaction-detail-form.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITransactionRes } from '@models/transaction.model';
 
 @Component({
   selector: 'app-update-transaction-detail-form',
   templateUrl: './update-transaction-detail-form.component.html',
-  styleUrls: ['./update-transaction-detail-form.component.css']
+  styleUrls: ['./update-transaction-detail-form.component.css'],
 })
 export class UpdateTransactionDetailFormComponent {
-
+  @Input({ required: true }) transactions!: ITransactionRes[];
 }
