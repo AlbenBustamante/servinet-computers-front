c4d486f9f0049356c7efd59fdd563a345756c727
Alben Bustamante <albenbustamante@gmail.com>
2025-05-03 17:15:37 -0500

    update: new cash transfer form logic and displaying messages

--- src/app/portal/pages/home/pages/transfers/components/new-cash-transfer-form/new-cash-transfer-form.component.ts
+++ src/app/portal/pages/home/pages/transfers/components/new-cash-transfer-form/new-cash-transfer-form.component.ts
@@ -19,10 +19,8 @@ export class NewCashTransferFormComponent {
   readonly receive = signal<boolean>(true);
   readonly selectedType = signal<CashBoxType>(CashBoxType.CASH_REGISTER);
   readonly selectedSafe = signal<ISafeDetailRes | undefined>(undefined);
-  readonly selectedSafeBase = computed(
-    () => this.selectedSafe()?.detailFinalBase
-  );
-  readonly availableAmount = signal<boolean>(true);
+  readonly availableAmount = signal<number>(0);
+  readonly selectedSafePropertyMessage = signal<string>('x');
   readonly availableTransfers;
   readonly cashTransfers;
   readonly form;
@@ -46,12 +44,26 @@ export class NewCashTransferFormComponent {
       cashBoxType: [CashBoxType.CASH_REGISTER, Validators.required],
       cashier: [0, [Validators.required, Validators.min(1)]],
       value: [, Validators.min(0)],
-      safeValue: [''],
+      safeAmount: [, Validators.min(1)],
+      safeDenomination: [''],
     });
+
+    this.form.get('safeAmount')?.disable();
+    this.form.get('safeDenomination')?.disable();
+  }
+
+  get selectedSafeBase() {
+    return this.selectedSafe()?.detailFinalBase;
+  }
+
+  get safeAmount() {
+    return this.form.get('safeAmount')?.value ?? 0;
   }
 
   setSelectedCashBox(event: Event) {
     if (this.selectedType() === CashBoxType.CASH_REGISTER) {
+      this.form.get('safeAmount')?.disable();
+      this.form.get('safeDenomination')?.disable();
       return this.selectedSafe.set(undefined);
     }
 
@@ -64,18 +76,56 @@ export class NewCashTransferFormComponent {
     if (index !== undefined && index > -1) {
       const safe = this.availableTransfers()?.safes[index];
       this.selectedSafe.set(safe);
+      this.form.get('safeAmount')?.enable();
+      this.form.get('safeDenomination')?.enable();
+      this.setAvailableAmount();
     }
   }
 
   setSelectedType(event: Event) {
     const target = event.target as HTMLSelectElement;
     this.selectedType.set(target.value as CashBoxType);
+
+    if (this.selectedType() === CashBoxType.SAFE) {
+      this.setAvailableAmount();
+    }
+  }
+
+  setAvailableAmount() {
+    this.availableAmount.set(0);
+    const safe = this.selectedSafeBase;
+
+    if (safe === undefined) {
+      return this.selectedSafePropertyMessage.set('Selecciona una caja fuerte');
+    }
+
+    const denomination = this.form.get('safeDenomination')?.value;
+
+    if (denomination === '') {
+      return this.selectedSafePropertyMessage.set(
+        'Selecciona una denominación'
+      );
+    }
+
+    this.availableAmount.set(safe![denomination as keyof IBase]);
+    this.selectedSafePropertyMessage.set(
+      `Cantidad disponible: ${this.availableAmount()}`
+    );
   }
 
   setReceive(event: Event) {
     const target = event.target as HTMLSelectElement;
     const value = target.value;
     this.receive.set(value === 'true');
+
+    if (this.receive() && this.selectedType() === CashBoxType.SAFE) {
+      this.form.patchValue({
+        safeAmount: undefined,
+        safeDenomination: '',
+      });
+
+      this.setAvailableAmount();
+    }
   }
 
   onSubmit() {
@@ -86,11 +136,6 @@ export class NewCashTransferFormComponent {
     this.setLoading(true);
 
     const createCashTransferDto = this.buildData();
-
-    if (!this.availableAmount()) {
-      console.error('amount not available');
-      return this.setLoading(false);
-    }
 
     this.cashTransferService.register(createCashTransferDto).subscribe({
       next: (cashTransfer) => {
@@ -108,10 +153,13 @@ export class NewCashTransferFormComponent {
 
   private resetForm() {
     this.form.reset();
-    this.form.get('receive')?.setValue('true');
-    this.form.get('cashBoxType')?.setValue(CashBoxType.CASH_REGISTER);
-    this.form.get('cashier')?.setValue(0);
-    this.form.get('safeValue')?.setValue('');
+    this.form.patchValue({
+      receive: 'true',
+      cashBoxType: CashBoxType.CASH_REGISTER,
+      cashier: 0,
+      safeAmount: undefined,
+      safeDenomination: undefined,
+    });
     this.receive.set(true);
     this.selectedType.set(CashBoxType.CASH_REGISTER);
   }
@@ -125,19 +173,18 @@ export class NewCashTransferFormComponent {
     let safeDetailId: number | undefined = undefined;
     let safeBase: IBase | undefined = undefined;
 
-    const safes = this.availableTransfers()!.safes;
-    const currentCashRegisterDetailId =
-      this.currentCashRegister()?.cashRegisterDetail.id!;
+    const { safes } = this.availableTransfers()!;
+    const { id } = this.currentCashRegister()?.cashRegisterDetail!;
 
     if (this.receive()) {
       receiverType = CashBoxType.CASH_REGISTER;
-      receiverId = currentCashRegisterDetailId;
+      receiverId = id;
       senderType = this.form.get('cashBoxType')?.value!;
       senderId = Number(this.form.get('cashier')?.value!);
       safeDetailId = senderType === CashBoxType.SAFE ? senderId : undefined;
     } else {
       senderType = CashBoxType.CASH_REGISTER;
-      senderId = currentCashRegisterDetailId;
+      senderId = id;
       receiverType = this.form.get('cashBoxType')?.value!;
       receiverId = Number(this.form.get('cashier')?.value!);
       safeDetailId = receiverType === CashBoxType.SAFE ? receiverId : undefined;
@@ -148,14 +195,16 @@ export class NewCashTransferFormComponent {
 
       if (safeIndex > -1) {
         safeBase = safes[safeIndex].detailFinalBase;
-        const property = this.form.get('safeValue')?.value! as keyof IBase;
+
+        const property = this.form.get('safeDenomination')
+          ?.value! as keyof IBase;
+
+        const amount = this.form.get('safeAmount')?.value! as number;
 
         if (this.receive()) {
-          this.availableAmount.set(safeBase[property] >= 1);
-          safeBase[property] -= safeBase[property] > 0 ? 1 : 0;
+          safeBase[property] -= safeBase[property] > 0 ? amount : 0;
         } else {
-          this.availableAmount.set(true);
-          safeBase[property] += 1;
+          safeBase[property] += amount;
         }
 
         const base = this.baseService.defaultBase();
@@ -175,7 +224,7 @@ export class NewCashTransferFormComponent {
       value,
       safeDetailId,
       safeBase,
-      currentCashRegisterDetailId,
+      currentCashRegisterDetailId: id,
     };
   }
 
