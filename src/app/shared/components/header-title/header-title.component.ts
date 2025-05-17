3b4cfa57f4dc8701370e64ca128b280b31213def
Alben Bustamante <albenbustamante@gmail.com>
2025-05-17 14:55:56 -0500

    update: header title component for return button

--- src/app/shared/components/header-title/header-title.component.ts
+++ src/app/shared/components/header-title/header-title.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { faLongArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header-title',
@@ -9,4 +11,15 @@ export class HeaderTitleComponent {
   @Input({ required: true }) hTitle!: string;
   @Input({ required: true }) hDescription!: string;
   @Input() hDescriptionSecondLine!: string;
+  @Input() return!: boolean;
+  readonly faReturn = faLongArrowLeft;
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router
+  ) {}
+
+  goToBack() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
 }
