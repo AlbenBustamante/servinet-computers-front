9d7d1eec1b23ec79cb63c5cc666be4d1cc867088
Alben Bustamante <albenbustamante@gmail.com>
2025-05-21 15:46:04 -0500

    feat: now a transfers can be deleted from admin page

--- src/app/admin/home/platforms/components/transfers-table/transfers-table.component.ts
+++ src/app/admin/home/platforms/components/transfers-table/transfers-table.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { ITable } from '@shared/components/custom-table/custom-table.component';
 import { DetailService } from '../../services/detail.service';
 import { CurrencyPipe, DatePipe } from '@angular/common';
+import { PlatformTransferService } from '@services/platform-transfer.service';
 
 @Component({
   selector: 'app-transfers-table',
@@ -28,12 +29,40 @@ export class TransfersTableComponent {
     ],
     body: this.service.transfers,
     noDataMessage: 'No se han realizado transferencias',
+    onRemove: (index) => this.remove(index),
   };
 
   constructor(
     private readonly service: DetailService,
+    private readonly platfromTransferService: PlatformTransferService,
     @Inject(LOCALE_ID) private readonly locale: string
   ) {
     this.loading = this.service.loading;
+  }
+
+  remove(index: number) {
+    const { id } = this.service.transfers()[index];
+
+    this.loading.set(true);
+
+    this.platfromTransferService.delete(id).subscribe({
+      next: () => {
+        this.service.transfers.update((prevValue) => {
+          const index = prevValue.findIndex((t) => t.id === id);
+
+          if (index > -1) {
+            prevValue.splice(index, 1);
+          }
+
+          return prevValue;
+        });
+
+        this.loading.set(false);
+      },
+      error: (err) => {
+        console.log(err);
+        this.loading.set(false);
+      },
+    });
   }
 }
