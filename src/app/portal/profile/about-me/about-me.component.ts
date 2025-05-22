d684081696e537e3adab4b0b0f99a9966366981f
Alben Bustamante <albenbustamante@gmail.com>
2025-05-22 18:21:42 -0500

    in progress: profile page

--- src/app/portal/profile/about-me/about-me.component.ts
+++ src/app/portal/profile/about-me/about-me.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-about-me',
-  templateUrl: './about-me.component.html'
+  templateUrl: './about-me.component.html',
 })
 export class AboutMeComponent {
+  readonly loggedIn;
+  readonly fullName = computed(
+    () => `${this.loggedIn()?.name} ${this.loggedIn()?.lastName}`
+  );
 
+  constructor(private readonly authService: AuthService) {
+    this.loggedIn = this.authService.loggedIn;
+  }
 }
