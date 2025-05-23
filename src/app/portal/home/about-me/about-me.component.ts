09e5695cc5528d04ed6fde5cf0a6f52905e021ef
Alben Bustamante <albenbustamante@gmail.com>
2025-05-23 15:45:00 -0500

    add: temporary info to my account page

--- src/app/portal/home/about-me/about-me.component.ts
+++ src/app/portal/home/about-me/about-me.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  readonly loggedIn;
+
+  constructor(private readonly authService: AuthService) {
+    this.loggedIn = this.authService.loggedIn;
+  }
+}
