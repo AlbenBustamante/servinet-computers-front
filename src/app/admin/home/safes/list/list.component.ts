73bc972fa282b2809bc5e541ba2ad270216454fe
Alben Bustamante <albenbustamante@gmail.com>
2025-05-23 16:24:21 -0500

    update: safes design and structure

--- src/app/admin/home/safes/list/list.component.ts
+++ src/app/admin/home/safes/list/list.component.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { SafeService } from '@services/safe.service';
 
 @Component({
-  selector: 'app-safes',
-  templateUrl: './safes.component.html',
-  styleUrls: ['./safes.component.css'],
+  selector: 'app-list',
+  templateUrl: './list.component.html',
 })
-export class SafesComponent {
+export class ListComponent {
+  readonly showRegisterForm = signal<boolean>(false);
   readonly loading = signal<boolean>(false);
 
   constructor(private readonly safeService: SafeService) {}
