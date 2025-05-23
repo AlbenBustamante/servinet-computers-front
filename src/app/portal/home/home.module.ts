7e40e1ebccf84b1a6664138040e478c4a41ea457
Alben Bustamante <albenbustamante@gmail.com>
2025-05-23 13:55:35 -0500

    feat: new table for journeys

--- src/app/portal/home/home.module.ts
+++ src/app/portal/home/home.module.ts
@@ -7,6 +7,8 @@ import { LayoutComponent } from './layout/layout.component';
 import { HomeNavItemComponent } from './components/home-nav-item/home-nav-item.component';
 import { HomeNavComponent } from './components/home-nav/home-nav.component';
 import { JourneysComponent } from './journeys/journeys.component';
+import { JourneysTableComponent } from './components/journeys-table/journeys-table.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,9 @@ import { JourneysComponent } from './journeys/journeys.component';
     LayoutComponent,
     HomeNavItemComponent,
     HomeNavComponent,
-    JourneysComponent
+    JourneysComponent,
+    JourneysTableComponent,
   ],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export default class HomeModule {}
--- src/app/portal/home/journeys/journeys.component.ts
+++ src/app/portal/home/journeys/journeys.component.ts
@@ -1,32 +1,33 @@
 import { formatDate } from '@angular/common';
 import { Component, Inject, LOCALE_ID, signal } from '@angular/core';
-import { ICashRegisterDetailRes } from '@models/cash-register.model';
-import { AuthService } from '@services/auth.service';
 import { UserService } from '@services/user.service';
+import { HomeService } from '../services/home.service';
+import { TokenService } from '@services/token.service';
 
 @Component({
   selector: 'app-journeys',
   templateUrl: './journeys.component.html',
 })
 export class JourneysComponent {
-  readonly loading = signal<boolean>(false);
-  readonly journeys = signal<ICashRegisterDetailRes[]>([]);
   readonly date = signal<Date>(new Date());
   readonly today = new Date();
-  readonly loggedIn;
+  readonly loading;
+  readonly journeys;
 
   constructor(
+    private readonly service: HomeService,
     private readonly userService: UserService,
-    private readonly authService: AuthService,
+    private readonly tokenService: TokenService,
     @Inject(LOCALE_ID) private readonly locale: string
   ) {
-    this.loggedIn = this.authService.loggedIn;
+    this.loading = this.service.loading;
+    this.journeys = this.service.journeys;
   }
 
   ngOnInit() {
     this.loading.set(true);
 
-    const { id } = this.loggedIn()!;
+    const { id } = this.tokenService.getInfo();
     const month = this.formatMonth(this.today);
 
     this.userService.getJourneys(id, month).subscribe({
