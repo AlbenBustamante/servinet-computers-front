50057011225e209a12af04c2f1168eba38d7a512
Alben Bustamante <albenbustamante@gmail.com>
2025-06-05 19:01:02 -0500

    update: loading, spinner components and add some tailwind utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "loading-text": "loading-text 1s ease-out infinite",
+      },
+      colors: {
+        body: {
+          DEFAULT: "rgb(var(--bg-body))",
+        },
+      },
+      keyframes: {
+        "loading-text": {
+          "0%, 100%": { color: colors.neutral["700"] },
+          "30%": { color: colors.neutral["500"] },
+        },
+      },
+    },
     fontFamily: {
       sans: ["Inter"],
     },
